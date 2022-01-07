019cdf2b1cde13e318a8d3fc35f631da688d4330
Higor Siqueira <higoraphaelhr@gmail.com>
2022-01-07 11:46:28 +0000

    teste

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,19 +3,32 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
+
 
 /**
  * Esta atividade permite ao usuário lançar um dado e ver o resultado  na tela
  */
+
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * Este método é chamado quando a atividade é criada
+     */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Encontre o botão no layout
         val rollButton: Button = findViewById(R.id.button)
+
+        //Defina um ouvinte de cliques no botão para lançar os dados quando o usuário tocar no botão
         rollButton.setOnClickListener { rollDice() }
+
+        // Faça um lançamento de dados quando o aplicativo iniciar
+        rollDice()
     }
 
     /**
@@ -26,14 +39,45 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
-        // Atualize a tela com o lançamento dos dados
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
-    }
-}
 
-class Dice(val numSides: Int) {
-    fun roll(): Int {
-        return (1..numSides).random()
+        // Atualize a tela com o lançamento dos dados
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        // Encontre o ImageView no layout
+        diceImage.setImageResource(R.drawable.dice_2)
+
+        // Determine qual ID de recurso drawable usar com base na jogada de dados
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        //Atualize o ImageView com o ID de recurso drawable correto
+        diceImage.setImageResource(drawableResource)
+
+        // Atualize a descrição do conteúdo
+        diceImage.contentDescription = diceRoll.toString()
+
+
+    }
+
+    /**
+     * Dados com um número fixo de lados.
+     */
+
+    class Dice(private val numSides: Int) {
+        /**
+         *Faça uma jogada de dados aleatória e retorne o resultado
+         */
+
+        fun roll(): Int {
+            return (1..numSides).random()
+
+        }
+
     }
 }
--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -2,16 +2,22 @@ package com.example.diceroller
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert.assertTrue
 
 /**
- * Example local unit test, which will execute on the development machine (host).
  *
- * See [testing documentation](http://d.android.com/tools/testing).
+ *
+ *
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number() {
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+
+        assertTrue("O valor de rollResult não estava entre 1 e 6",rollResult in 1..6)
+
     }
+
 }
